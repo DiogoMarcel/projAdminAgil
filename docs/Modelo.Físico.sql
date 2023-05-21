a4e5dfe64abbfcf342dcef001e0387f018fc111d
DiogoMarcel <diogo.dmds@gmail.com>
2023-05-21 18:28:24 -0300

    docs novos

--- docs/Modelo.Físico.sql
+++ docs/Modelo.Físico.sql
@@ -1,196 +1,205 @@
-BEGIN; teste
+begin;
+-----------
 
-CREATE TABLE usuario 
+DROP TABLE cargo CASCADE;
+DROP TABLE empresa CASCADE;
+DROP TABLE equipe CASCADE;
+DROP TABLE fatores CASCADE;
+DROP TABLE funcao CASCADE;
+DROP TABLE incremento CASCADE;
+DROP TABLE melhorar CASCADE;
+DROP TABLE meta CASCADE;
+DROP TABLE pergunta CASCADE;
+DROP TABLE pesquisa CASCADE;
+DROP TABLE quefoibom CASCADE;
+DROP TABLE quefoiruim CASCADE;
+DROP TABLE respostausuario CASCADE;
+DROP TABLE retrospectiva CASCADE;
+DROP TABLE retrospectiva_sprint CASCADE;
+DROP TABLE sprint CASCADE;
+DROP TABLE usuario CASCADE;
+DROP TABLE usuario_cargo CASCADE;
+DROP TABLE usuario_empresa CASCADE;
+DROP TABLE usuario_equipe CASCADE;
+DROP TABLE usuario_funcao CASCADE;
+
+-----------
+
+create table equipe 
 ( 
- id_usuario bigserial NOT NULL,  
- apelido VARCHAR(50),  
- usuario VARCHAR(50) NOT NULL,  
- senha VARCHAR(50) NOT NULL,  
- gerenciaPesquisa boolean NOT NULL default false,  
- gerenciaUsuario boolean NOT NULL default false,   
- UNIQUE (usuario),
- PRIMARY KEY (id_usuario)
+ id_equipe bigserial primary key,
+ nome varchar(50)
 ); 
 
-CREATE TABLE empresa 
+create table empresa 
 ( 
- id_empresa bigserial NOT NULL,
- nome VARCHAR(50) NOT NULL,
- PRIMARY KEY (id_empresa)
+ id_empresa bigserial not null primary key,
+ nome varchar(50)
 ); 
 
-CREATE TABLE cargo 
+create table funcao 
 ( 
- descricao VARCHAR(50) NOT NULL,  
- id_cargo bigserial NOT NULL,
- PRIMARY KEY (id_cargo)
+ id_funcao bigserial not null primary key,
+ descricao varchar(50)
 ); 
 
-CREATE TABLE funcao 
+create table cargo 
 ( 
- descricao VARCHAR(50) NOT NULL,  
- id_funcao bigserial NOT NULL,
- PRIMARY KEY (id_funcao)
+ id_cargo bigserial not null primary key,
+ descricao varchar(50)
 ); 
 
-CREATE TABLE equipe 
+create table colaborador 
 ( 
- id_equipe bigserial NOT NULL,
- nome VARCHAR(50) NOT NULL,
- PRIMARY KEY (id_equipe) 
+ id_colaborador bigserial not null primary key,
+ usuario varchar(50),
+ senha varchar(50),
+ apelido varchar(50),
+ gerenciapesquisa boolean,
+ gerenciausuario boolean,
+ unique (usuario)
+);
+
+create table sprint 
+( 
+ id_sprint bigserial not null primary key,
+ nome varchar(50),
+ datafinal date not null,
+ datainicio date not null 
 ); 
 
-CREATE TABLE sprint 
+create table pesquisa 
 ( 
- id_sprint bigserial NOT NULL,
- nome VARCHAR(50) NOT NULL,  
- dataFinal date,  
- dataInicio date ,
- PRIMARY KEY (id_sprint) 
+ id_pesquisa bigserial not null primary key,
+ titulo varchar(50)
 ); 
 
-CREATE TABLE retrospectiva 
+create table retrospectiva 
 ( 
- alcancado boolean NOT NULL default false,  
- nome VARCHAR(50) NOT NULL,  
- id_retrospectiva bigserial NOT NULL,
- PRIMARY KEY (id_retrospectiva)
+ id_retrospectiva bigserial not null primary key,
+ metaalcancada boolean not null default false,
+ idsprint bigint,
+ idcfgretrospectiva bigint
 ); 
 
-CREATE TABLE pesquisa 
+create table pesquisa_pergunta 
 ( 
- id_pesquisa bigserial NOT NULL,
- titulo VARCHAR(50) NOT NULL,
- PRIMARY KEY (id_pesquisa) 
+ id_pesquisapergunta bigserial not null primary key,
+ pergunta varchar(300),
+ valorfinal numeric(5,2),
+ valorinicial numeric(5,2),
+ tiporesposta char not null default 'd',
+ tamanhototal int,
+ idpesquisa bigint,
+ check (tiporesposta like '%d, s, b%')
 ); 
 
-CREATE TABLE pergunta 
+create table retrospectiva_item 
 ( 
- id_pergunta bigserial NOT NULL,
- pergunta VARCHAR(50) NOT NULL,  
- tiporesposta CHAR NOT NULL DEFAULT 'd',
- valorInicial VARCHAR(50),  
- valorFinal VARCHAR(50),  
- tamanhoTotal INT,  
- id_pesquisa bigint,
- CHECK (tiporesposta LIKE '%d, s, b%'),
- PRIMARY KEY (id_pergunta)
+ id_retrospectivaitem bigserial not null primary key,
+ idretrospectiva bigint,
+ idcfgretrospectivaitem bigint
 ); 
 
-CREATE TABLE melhorar 
+create table cfgretrospectiva_item 
 ( 
- id_melhorar bigserial NOT NULL,
- descricao VARCHAR(50) NOT NULL,  
- id_retrospectiva bigint,
- PRIMARY KEY (id_melhorar) 
+ id_cfgrestrospectivaitem bigserial not null primary key,
+ descricao varchar(50),
+ idcfgretrospectiva bigint
 ); 
 
-CREATE TABLE queFoiRuim 
+create table cfgretrospectiva 
 ( 
- id_queFoiRuim bigserial NOT NULL,
- descricao VARCHAR(50) NOT NULL,
- id_retrospectiva bigint ,
- PRIMARY KEY (id_queFoiRuim) 
+ id_cfgretrospectiva bigserial not null primary key,
+ nome varchar(50)
 ); 
 
-CREATE TABLE queFoiBom 
+create table sprint_equipe 
 ( 
- id_queFoiBom bigserial NOT NULL,
- descricao VARCHAR(50) NOT NULL,
- id_retrospectiva bigint,
- PRIMARY KEY (id_queFoiBom)  
+ id_sprintequipe bigserial not null primary key,
+ idsprint bigint,
+ idequipe bigint
 ); 
 
-CREATE TABLE fatores 
+create table sprint_participante 
 ( 
- id_fatores bigserial NOT NULL,
- descricao VARCHAR(50) NOT NULL,
- id_retrospectiva bigint,
- PRIMARY KEY (id_fatores)
+ id_sprintparticipante bigserial not null primary key,
+ idsprintequipe bigint,
+ idcolaborador bigint
+);
+
+create table resposta_pesquisa 
+( 
+ id_respostapesquisa bigserial not null primary key,
+ respostastring varchar(300),
+ respostadouble numeric(5,2),
+ respostaboolean boolean,
+ idsprintparticipante bigint,
+ idpesquisapergunta bigint
 ); 
 
-CREATE TABLE incremento 
+create table cards_retrospectivaitem 
 ( 
- id_incremento bigserial NOT NULL,
- descricao VARCHAR(50) NOT NULL,
- id_retrospectiva bigint,
- PRIMARY KEY (id_incremento)  
+ id_cardsretrospectivaitem bigserial not null primary key,
+ descricao varchar(50),
+ idretrospectivaitem bigint
 ); 
 
-CREATE TABLE meta 
+create table colaborador_empresa 
 ( 
- id_meta bigserial NOT NULL,
- descricao VARCHAR(50) NOT NULL,
- id_retrospectiva bigint ,
- PRIMARY KEY (id_meta) 
+ id_empresa bigint,
+ id_colaborador bigint,
+ primary key (id_empresa, id_colaborador)
 ); 
 
-CREATE TABLE usuario_equipe 
+create table colaborador_funcao 
 ( 
- id_equipe bigint,  
- id_usuario bigint,
- primary key (id_equipe, id_usuario)
+ id_funcao bigint,
+ id_colaborador bigint,
+ primary key (id_funcao, id_colaborador)
 ); 
 
-CREATE TABLE usuario_empresa 
+create table colaborador_cargo 
 ( 
- id_empresa bigint,  
- id_usuario bigint,
- primary key (id_empresa, id_usuario) 
+ id_cargo bigint,
+ id_colaborador bigint,
+ primary key (id_cargo, id_colaborador)
 ); 
 
-CREATE TABLE usuario_cargo 
+create table equipe_colaborador 
 ( 
- id_cargo bigint,  
- id_usuario bigint,
- primary key (id_cargo, id_usuario)
+ id_equipe bigint,
+ id_colaborador bigint,
+ primary key (id_equipe, id_colaborador)
 ); 
 
-CREATE TABLE usuario_funcao 
+create table sprint_pesquisa 
 ( 
- id_funcao bigint,  
- id_usuario bigint,
- primary key (id_funcao, id_usuario) 
+ id_sprintpesquisa bigserial not null primary key,
+ idpesquisa bigint,
+ idsprint bigint
 ); 
 
-CREATE TABLE repostaUsuario 
-( 
- respostaDouble NUMERIC(18,2),  
- respostaString VARCHAR(50),  
- respostaBoolean BOOLEAN,  
- id_usuario bigint,  
- id_sprint bigint,  
- id_pergunta bigint,  
- id_pesquisa bigint,
- id_respostaUsuario bigserial NOT NULL,
- primary key (id_respostaUsuario)
-); 
-
-CREATE TABLE retrospectiva_sprint 
-( 
- id_retrospectiva bigint,  
- id_sprint bigint,
- primary key (id_retrospectiva, id_sprint)  
-); 
-
-ALTER TABLE melhorar 				ADD FOREIGN KEY(id_retrospectiva) 	REFERENCES retrospectiva 	(id_retrospectiva);
-ALTER TABLE queFoiRuim 				ADD FOREIGN KEY(id_retrospectiva) 	REFERENCES retrospectiva 	(id_retrospectiva);
-ALTER TABLE queFoiBom 				ADD FOREIGN KEY(id_retrospectiva) 	REFERENCES retrospectiva 	(id_retrospectiva);
-ALTER TABLE fatores 				ADD FOREIGN KEY(id_retrospectiva) 	REFERENCES retrospectiva 	(id_retrospectiva);
-ALTER TABLE incremento 				ADD FOREIGN KEY(id_retrospectiva) 	REFERENCES retrospectiva 	(id_retrospectiva);
-ALTER TABLE meta 					ADD FOREIGN KEY(id_retrospectiva) 	REFERENCES retrospectiva 	(id_retrospectiva);
-ALTER TABLE usuario_equipe 			ADD FOREIGN KEY(id_equipe)  		REFERENCES equipe 			(id_equipe);
-ALTER TABLE usuario_equipe 			ADD FOREIGN KEY(id_usuario) 		REFERENCES usuario  		(id_usuario);
-ALTER TABLE usuario_empresa			ADD FOREIGN KEY(id_empresa) 		REFERENCES empresa  		(id_empresa);
-ALTER TABLE usuario_empresa			ADD FOREIGN KEY(id_usuario) 		REFERENCES usuario  		(id_usuario);
-ALTER TABLE usuario_cargo  			ADD FOREIGN KEY(id_cargo)   		REFERENCES cargo 			(id_cargo);
-ALTER TABLE usuario_cargo  			ADD FOREIGN KEY(id_usuario) 		REFERENCES usuario  		(id_usuario);
-ALTER TABLE usuario_funcao 			ADD FOREIGN KEY(id_funcao)  		REFERENCES funcao 			(id_funcao);
-ALTER TABLE usuario_funcao 			ADD FOREIGN KEY(id_usuario) 		REFERENCES usuario  		(id_usuario);
-ALTER TABLE repostaUsuario 			ADD FOREIGN KEY(id_usuario) 		REFERENCES usuario  		(id_usuario);
-ALTER TABLE repostaUsuario 			ADD FOREIGN KEY(id_sprint)  		REFERENCES sprint 			(id_sprint);
-ALTER TABLE repostaUsuario 			ADD FOREIGN KEY(id_pergunta)		REFERENCES pergunta 		(id_pergunta);
-ALTER TABLE repostaUsuario 			ADD FOREIGN KEY(id_pesquisa)		REFERENCES pesquisa 		(id_pesquisa);
-ALTER TABLE retrospectiva_sprint 	ADD FOREIGN KEY(id_retrospectiva) 	REFERENCES retrospectiva 	(id_retrospectiva);
-ALTER TABLE retrospectiva_sprint 	ADD FOREIGN KEY(id_sprint)        	REFERENCES sprint 			(id_sprint);
-ALTER TABLE pergunta 				ADD FOREIGN KEY(id_pesquisa)        REFERENCES pesquisa			(id_pesquisa);
+alter table retrospectiva 			add foreign key(idsprint) 				references sprint (id_sprint);
+alter table retrospectiva 			add foreign key(idcfgretrospectiva) 	references cfgretrospectiva (id_cfgretrospectiva);
+alter table pesquisa_pergunta 		add foreign key(idpesquisa) 			references pesquisa (id_pesquisa);
+alter table retrospectiva_item 		add foreign key(idretrospectiva) 		references retrospectiva (id_retrospectiva);
+alter table retrospectiva_item 		add foreign key(idcfgretrospectivaitem) references cfgretrospectiva_item (id_cfgrestrospectivaitem);
+alter table cfgretrospectiva_item 	add foreign key(idcfgretrospectiva) 	references cfgretrospectiva (id_cfgretrospectiva);
+alter table sprint_equipe 			add foreign key(idsprint) 				references sprint (id_sprint);
+alter table sprint_equipe 			add foreign key(idequipe) 				references equipe (id_equipe);
+alter table sprint_participante 	add foreign key(idsprintequipe) 		references sprint_equipe (id_sprintequipe);
+alter table sprint_participante 	add foreign key(idcolaborador) 			references colaborador (id_colaborador);
+alter table resposta_pesquisa 		add foreign key(idsprintparticipante) 	references sprint_participante (id_sprintparticipante);
+alter table resposta_pesquisa 		add foreign key(idpesquisapergunta) 	references pesquisa_pergunta (id_pesquisapergunta);
+alter table cards_retrospectivaitem add foreign key(idretrospectivaitem) 	references retrospectiva_item (id_retrospectivaitem);
+alter table colaborador_empresa 	add foreign key(id_empresa) 			references empresa (id_empresa);
+alter table colaborador_empresa 	add foreign key(id_colaborador) 		references colaborador (id_colaborador);
+alter table colaborador_funcao 		add foreign key(id_funcao) 				references funcao (id_funcao);
+alter table colaborador_funcao 		add foreign key(id_colaborador) 		references colaborador (id_colaborador);
+alter table colaborador_cargo 		add foreign key(id_cargo) 				references cargo (id_cargo);
+alter table colaborador_cargo 		add foreign key(id_colaborador) 		references colaborador (id_colaborador);
+alter table equipe_colaborador 		add foreign key(id_equipe) 				references equipe (id_equipe);
+alter table equipe_colaborador 		add foreign key(id_colaborador) 		references colaborador (id_colaborador);
+alter table sprint_pesquisa 		add foreign key(idpesquisa) 			references pesquisa (id_pesquisa);
+alter table sprint_pesquisa 		add foreign key(idsprint) 				references sprint (id_sprint);
